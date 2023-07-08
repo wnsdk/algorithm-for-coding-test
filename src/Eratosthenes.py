f9625146010329b76e4e949df05a8fb5ca9bfcec
Juna Choi <jn307742@gmail.com>
2023-07-08 13:14:49 +0900

    Update Eratosthenes.py

--- src/Eratosthenes.py
+++ src/Eratosthenes.py
@@ -1,10 +1,10 @@
 n = 1000
-a = [False, False] + [True] * (n - 1)
-primes = []
+chk = [False, False] + [True] * n
 
-for i in range(2, n + 1):
-    if a[i]:
+for i in range(2, int(n ** 0.5) + 1):
+    if chk[i]:
         # i를 제외한 i의 배수를 제외하기
-        primes.append(i)
-        for j in range(2 * i, n + 1, i):
-            a[j] = False
+        for j in range(2 * i, n, i):
+            chk[j] = False
+            
+primes = [i ** 2 for i in range(2, n) if chk[i]]
